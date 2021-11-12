9d88aeee95810e190ce5eb6fe4aad51561ef7cc1
Surya B <58516233+surya-b21@users.noreply.github.com>
2021-11-12 09:50:49 +0700

    mengganti logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{asset('storage/logo/logo-surakarta.png')}}" class="mx-auto pt-3" alt="logo-surakarta">
+<img src="{{asset('logo/logo-surakarta.png')}}" class="mx-auto pt-3" alt="logo-surakarta">
