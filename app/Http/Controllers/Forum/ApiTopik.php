4dbe24022c3cf1f2f7f17f55959bae124b253c7a
Wulandr <wdari0161@gmail.com>
2021-11-17 11:29:54 +0700

    menambahkan api untuk bagian forum

---
+++ app/Http/Controllers/Forum/ApiTopik.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Forum;
+
+use App\Models\Forum\Topik;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use mysqli;
+
+class ApiTopik extends Controller
+{
+    public function index()
+    {
+        $topik = Topik::all();
+        return response()->json([
+            "status" => true,
+            "message" => "Topik",
+            "data" => $topik
+        ], 200);
+    }
+    public function showimage($id)
+    {
+        // $topik = Topik::all()->where('id', $id);
+        $topik[] = Topik::all();
+        $top = Topik::all();
+        $row = count($top);
+        for ($i = 0; $i < 2; $i++) {
+            if ($topik[0][$i]['id'] == $id) {
+                //  return ($topik[0][$i]['foto_path']);
+                return response()->file(public_path(
+                    'storage/' . $topik[0][$i]['foto_path']
+                ));
+            }
+        }
+        // return ($topik[0][$id]['foto_url']);
+
+        // return response()->file(public_path(
+        //     'storage/' . $topik[$index]['foto_url']
+        // ));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Forum\ApiForum;
+use App\Http\Controllers\Forum\ApiKomentar;
+use App\Http\Controllers\Forum\ApiTopik;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/kategori', [ApiForum::class, 'index']);
+Route::get('/kategori/showparent', [ApiForum::class, 'showparent']);
+Route::get('/topik/showimage/{id?}', [ApiTopik::class, 'showimage']);
+Route::get('/kategori/showchild/{parent?}', [ApiForum::class, 'showchild']);
+Route::get('/topik', [ApiTopik::class, 'index']);
+Route::get('/komentar', [ApiKomentar::class, 'index']);
