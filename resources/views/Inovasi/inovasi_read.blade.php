8ecae80bc516b1f7c603bcd6cdc30c50f300ccff
Wulandr <wdari0161@gmail.com>
2021-12-23 22:18:53 +0700

    menampilkan dokumen di inovasi - rev

--- resources/views/Inovasi/inovasi_read.blade.php
+++ resources/views/Inovasi/inovasi_read.blade.php
@@ -103,14 +103,16 @@
                         <?php
                         if ($dokumen['id_inovasi'] == $data['id']) {
                             $doc = $dokumen['file_path']; ?>
-                            <a href="{{Storage::url($doc)}}"><img src="https://cdn.pixabay.com/photo/2017/03/08/21/19/file-2127825_960_720.png" class="rounded" alt="" width="30px" height="30px"></a>
-                    </div>
-                    <div style="display: inline-block; margin-left:5px;">
-                        <a href="{{Storage::url($doc)}}">Lihat Dokumen</a>
-                    </div>
+                            <div style="display: inline-block;">
+                                <a href="{{Storage::url($doc)}}"><img src="https://cdn.pixabay.com/photo/2017/03/08/21/19/file-2127825_960_720.png" class="rounded" alt="" width="30px" height="30px"></a>
+                            </div>
+                            <div style="display: inline-block; margin-left:5px;">
+                                <a href="{{Storage::url($doc)}}">Lihat Dokumen</a>
+                            </div>
 
-                <?php } ?>
-                @endforeach
+                        <?php } ?>
+                    </div>
+                    @endforeach
 
                 </div>
                 <div class="mt-4">
